cf640a9a32588ef5f552b815d6296cadb2893c42
Zio <zi.ao.tan@outlook.com>
2021-07-26 12:53:05 -0700

    finish building the login forms

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import ListItem from './app/components/ListItem';
 import Screen from './app/components/Screen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import LoginScreen from './app/screens/LoginScreen';
 
 import MessagesScreen from './app/screens/MessagesScreen';
 
@@ -21,18 +22,7 @@ const categories = [
 
 const App = () => {
   const [category, setCategory] = useState(categories[0]);
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectedItem={setCategory}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 };
 
 export default App;
