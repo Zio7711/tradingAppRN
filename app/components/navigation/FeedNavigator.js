9e70ad25c8a8203535815db1e26cb953ab5e8728
Zio <zi.ao.tan@outlook.com>
2021-07-27 15:32:26 -0700

    beautify the tabs

--- app/components/navigation/FeedNavigator.js
+++ app/components/navigation/FeedNavigator.js
@@ -7,7 +7,7 @@ const Stack = createStackNavigator();
 
 const FeedNavigator = () => {
   return (
-    <Stack.Navigator mode="modal">
+    <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Listings" component={ListingsScreen} />
       <Stack.Screen
         name="ListingDetails"
