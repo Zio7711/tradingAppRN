402671d6baca1d1e1c569282ba4b0025123ce5ed
Zio <zi.ao.tan@outlook.com>
2021-07-27 14:48:28 -0700

    build appNavigator

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import AppNavigator from './app/components/navigation/AppNavigator';
 import AuthNavigator from './app/components/navigation/AuthNavigation';
 import NavigationTheme from './app/components/navigation/NavigationTheme';
 
 const App = () => {
   return (
     <NavigationContainer theme={NavigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 };
