a8ec0c9c3b4d6f8c5f0c6a1b4fc3d095b0869615
Zio <zi.ao.tan@outlook.com>
2021-07-27 15:03:29 -0700

    build feedNavigator component

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -21,7 +21,7 @@ const listings = [
   },
 ];
 
-const ListingsScreen = () => {
+const ListingsScreen = ({ navigation }) => {
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -32,6 +32,7 @@ const ListingsScreen = () => {
             title={item.title}
             subTitle={'$' + item.price}
             image={item.image}
+            onPress={() => navigation.navigate('ListingDetails', item)}
           />
         )}
       />
