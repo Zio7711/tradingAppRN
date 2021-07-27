81e87f8f76cdf9a13ed15f6bbb49a8bceacc7d1c
Zio <zi.ao.tan@outlook.com>
2021-07-26 17:15:52 -0700

    handle long text

--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
 import defaultStyles from '../../config/style';
-const AppText = ({ children, style }) => {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+const AppText = ({ children, style, ...otherProps }) => {
+  return (
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 };
 
 export default AppText;
