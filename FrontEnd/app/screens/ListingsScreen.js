1d43a720636317627bf194fe25d3d4bc43858486
Zio <zi.ao.tan@outlook.com>
2021-08-11 10:09:56 -0700

    add animation to loading screen

--- FrontEnd/app/screens/ListingsScreen.js
+++ FrontEnd/app/screens/ListingsScreen.js
@@ -1,32 +1,33 @@
 import React, { useEffect } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import Button from '../components/AppButton';
-
 import Card from '../components/Card';
 import client from '../api/client';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
 import routes from '../components/navigation/routes';
-import listingsApi from '../api/listings';
 import { useState } from 'react';
 import AppText from '../components/AppText/AppText';
+import ActivityIndicator from '../components/ActivityIndicator';
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   const loadListings = async () => {
-    // const response = await listingsApi.getListings;
+    setLoading(true);
     client
       .get('/listings')
       .then((res) => {
         setListings(res.data);
         setError(false);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
         setError(true);
+        setLoading(false);
       });
   };
 
@@ -42,6 +43,8 @@ const ListingsScreen = ({ navigation }) => {
           <Button title="Retry" onPress={loadListings}></Button>
         </>
       )}
+      <ActivityIndicator visible={loading} />
+
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
