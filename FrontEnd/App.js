435943b6b74cf3d61df88f3a3c8df0fd4ed0b8c5
Zio <zi.ao.tan@outlook.com>
2021-08-16 12:12:01 -0700

    install async storage

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -7,10 +7,11 @@ import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import { Button } from 'react-native';
 
 const App = () => {
-  // const netInfo = useNetInfo();
+  const netInfo = useNetInfo();
   return (
     <NavigationContainer theme={NavigationTheme}>
       <AppNavigator />
+      <Button disabled={!netInfo.isInternetReachable} title="here" />
     </NavigationContainer>
   );
 };
