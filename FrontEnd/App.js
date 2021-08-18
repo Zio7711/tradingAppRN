b5a133fd60cf1027bce85ce8981aeabfd3c660c7
Zio <zi.ao.tan@outlook.com>
2021-08-18 16:41:33 -0700

    store the current user

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,19 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
+import AuthContext from './app/auth/context';
 import AppNavigator from './app/components/navigation/AppNavigator';
+
 import AuthNavigator from './app/components/navigation/AuthNavigation';
 import NavigationTheme from './app/components/navigation/NavigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
 
 const App = () => {
+  const [user, setUser] = useState();
   return (
-    <>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
       <NavigationContainer theme={NavigationTheme}>
-        {/* <AppNavigator style={{ zIndex: -1 }} /> */}
-        <AuthNavigator />
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
-    </>
+    </AuthContext.Provider>
   );
 };
 
--- FrontEnd/app/screens/LoginScreen.js
+++ FrontEnd/app/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Image, StyleSheet } from 'react-native';
 import Screen from '../components/Screen';
 import * as Yup from 'yup';
@@ -11,6 +11,7 @@ import {
   SubmitButton,
 } from '../components/forms';
 import authApi from '../api/auth';
+import AuthContext from '../auth/context';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -18,6 +19,8 @@ const validationSchema = Yup.object().shape({
 });
 
 const LoginScreen = (props) => {
+  const authContext = useContext(AuthContext);
+
   const [loginFailed, setLoginFailed] = useState(false);
   const handleSubmit = ({ email, password }) => {
     authApi
@@ -25,7 +28,7 @@ const LoginScreen = (props) => {
       .then((res) => {
         setLoginFailed(false);
         const user = jwtDecode(res.data);
-        console.log(user);
+        authContext.setUser(user);
       })
       .catch(() => {
         return setLoginFailed(true);
