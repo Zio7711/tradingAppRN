1c111bce53e18b316f8a86f03427ae44fdc77e55
Zio <zi.ao.tan@outlook.com>
2021-08-18 17:37:19 -0700

    control the splash screen

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -4,6 +4,7 @@ import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
 import AppNavigator from './app/components/navigation/AppNavigator';
 import jwtDecode from 'jwt-decode';
+import AppLoading from 'expo-app-loading';
 
 import AuthNavigator from './app/components/navigation/AuthNavigation';
 import NavigationTheme from './app/components/navigation/NavigationTheme';
@@ -11,6 +12,7 @@ import OfflineNotice from './app/components/OfflineNotice';
 
 const App = () => {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -18,9 +20,19 @@ const App = () => {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => {
+          setIsReady(true);
+        }}
+        onError={() => {
+          console.log('error in appLoading component');
+        }}
+      />
+    );
+  }
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
