4c3fab291674f6d92c73a25ffe7a6e1b52e9347b
Zio <zi.ao.tan@outlook.com>
2021-08-18 18:01:33 -0700

    modify client file

--- FrontEnd/app/api/client.js
+++ FrontEnd/app/api/client.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
-import cache from '../utility/cache';
+import authStorage from '../auth/storage';
+// import cache from '../utility/cache';
 const client = axios.create({
   baseURL: 'http://172.17.171.67:8000/api',
   timeout: 1000,
+});
+
+client.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers['x-auth-token'] = authToken;
 });
 
 //below is the async cache layer which failed
